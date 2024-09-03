2895344ca6ef03b159906520d43886698839419c
chaitanya <chaitanyaarora3@gmail.com>
2024-09-03 10:50:23 +0530

    sm8450-common: Inherit Basic Call Recorder

--- common.mk
+++ common.mk
@@ -95,6 +95,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+#Bcr
+$(call inherit-product, vendor/bcr/bcr.mk)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl:64
