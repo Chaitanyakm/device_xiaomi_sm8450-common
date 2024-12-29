9e908b032548a1c949ed30277864021d0d82be29
pabloescobar-reborn <yogivangala07@gmail.com>
2024-12-29 10:07:53 +0000

    sm8450-common: parts: Switch back to thermal profiles nuked thermal tile service

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -34,7 +34,6 @@ import android.view.Display.HdrCapabilities;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
-import org.lineageos.settings.thermal.ThermalTileService;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
