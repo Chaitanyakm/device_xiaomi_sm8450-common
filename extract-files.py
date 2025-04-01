ca8c10aea28f621d7e7aa4c7bce8a1ab9f79edba
LuK1337 <priv.luk@gmail.com>
2025-04-01 16:11:23 +0200

    sm8450-common: Add libwfdservice_shim for WFD
    e69a5be2449122f90220151f9d01ace15221facf | AOSP in system/hardware/interfaces
    broke the ABI ;'(
    
    Change-Id: I3badfc56581a6bf3de5d7e996fa51f59eb164aae

--- extract-files.py
+++ extract-files.py
@@ -57,6 +57,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/bin/wfdservice64': blob_fixup()
+        .add_needed('libwfdservice_shim.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
