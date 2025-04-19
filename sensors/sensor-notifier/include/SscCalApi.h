678cf852a7e296513f6708e8b643ef2dfaf2c4fb
Pabloescobar-reborn <pabloescobarreborn77@gmail.com>
2025-04-19 21:21:07 +0530

    sm8450-common: sensor: Increase size of unused array in _oem_msg to match stock padding
    Increased the size of the  array in the  struct from 16 to 64.
    This adjustment ensures compatibility with the stock sensor-notifier implementation, which seems to use a larger padded struct.
    Fixes SIGSEGV (segmentation fault) errors caused by mismatched memory layouts.

--- sensors/sensor-notifier/include/SscCalApi.h
+++ sensors/sensor-notifier/include/SscCalApi.h
@@ -23,7 +23,7 @@ struct _oem_msg {
     float unknown2;
     float notifyTypeFloat;
     float value;
-    float unused[16];
+    float unused[64];
 };
 
 typedef void (*init_current_sensors_t)(bool debug);
