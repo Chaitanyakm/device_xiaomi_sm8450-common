1e29a7c373ea9c34a5487047e883ecb2d6ce4b3b
Arian <arian.kulmer@web.de>
2025-07-11 16:04:09 +0200

    sm8450-common: Don't explicitly build libwfdaac_vendor
    This has been converted to blueprint and is built implicitly as
    dependency of libwfdmmsrc_proprietary.so.
    
    Change-Id: I82bcca5f9aa85793c7b5a366e35472997e57feaf

--- common.mk
+++ common.mk
@@ -485,10 +485,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libwfdaac_vendor:64
-
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink \
