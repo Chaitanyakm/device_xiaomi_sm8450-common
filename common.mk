b11d4ff33e3541eb4cb40e69cfa709b4af642d2a
ralph950412 <ralph950412@gmail.com>
2026-01-06 01:00:10 +0530

    sm8450-common: Implement torch light control
    [RealJohnGalt: Modify for oneplus usage and add toggle switch]
    [cyberknight777/ralph950412: Adapt nodes and SEPolicy for Xiaomi SM8350]
    [rk134: Adapt nodes/sepolicy for Xiaomi SM8450]
    
    Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: rk134 <rahul@aospa.co>

--- common.mk
+++ common.mk
@@ -125,6 +125,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.xiaomi_sm8450)
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
