e6f1cc781a3c2a497186ae0fc62365542f84f911
binbin323 <bin@dhao.cc>
2026-06-24 20:21:40 +0900

    sm8450-common: Patch libwfdmmsrc_system with libgui_shim

--- extract-files.py
+++ extract-files.py
@@ -120,6 +120,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
