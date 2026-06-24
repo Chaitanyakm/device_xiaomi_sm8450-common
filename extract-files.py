a19756ab94d6c855d0e91a62180470b748547145
binbin323 <bin@dhao.cc>
2026-06-25 01:32:14 +0900

    sm8450-common: Patch wfdservice with libaudiobase

--- extract-files.py
+++ extract-files.py
@@ -119,6 +119,7 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libaudiobase.so')
