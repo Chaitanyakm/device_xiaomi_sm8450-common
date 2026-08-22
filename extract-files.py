6f0397297048eb204ba41dcd1ba36fef8e8f8a03
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-08-22 18:14:41 +0530

    sm8450-common: Update WFD system stack from LA.QSSI.17.0.r1-06700-qssi.0
    - Original source > https://github.com/ThankYouMario/proprietary_vendor_qcom_common/tree/calcite
    - Only 64bit source > https://gitlab.com/vendor-trees/blobs/-/commit/4e58f9257e1361ca4b9deb61a8472918f50a8458
    
    Change-Id: I12afa4cfd60e7297e998b669ed7aa0058d7ad14b
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- extract-files.py
+++ extract-files.py
@@ -48,6 +48,12 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/etc/init/wfdservice.rc': blob_fixup()
+        .regex_replace(r'(start|stop) wfdservice\b', r'\1 wfdservice64'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
+    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
     (
         'vendor/etc/msm_irqbalance.conf'
     ): blob_fixup()
